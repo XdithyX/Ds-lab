127a99ac8cf9dc9f7fd5bdbe86c40889e29238f6
XdithyX <83907264+XdithyX@users.noreply.github.com>
2021-12-13 07:03:24 +0000

    Create transposesparse.c

---
+++ transposesparse.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+struct matrix{
+	int row;
+	int col;
+	int val;
+};
+
+void read(struct matrix m[]){
+       int n,col,row;
+	printf("Enter the no of rows and columns in the matrix: ");
+	scanf("%d%d",&row,&col);
+	m[0].row=row;
+	m[0].col=col;
+	
+	int i,j,k=1,temp,count=0;
+	printf("Enter the Matrix elements:\n");	
+	for(i=0;i<m[0].row;i++){
+		for(j=0; j<m[0].col;j++){
+			scanf("%d", &temp);
+			if(temp!=0){
+				m[k].row=i;
+				m[k].col=j;
+				m[k++].val=temp;
+				count+=1;
+			}
+		}
+	}
+	m[0].val=count;
+	
+}
+
+void printMatrix(struct matrix m[]){
+	int i;
+	printf("\n\n\nrow\tcol\tval\n");
+	for(i=0;i<m[0].val+1;i++)
+		printf("%d\t%d\t%d\n",m[i].row,m[i].col,m[i].val);
+	printf("\n\n\n");
+}
+
+void transpose(struct matrix m[],struct matrix res[])
+{ int i,j,k=1;
+ res[0].row=m[0].col;
+ res[0].col=m[0].row;
+ res[0].val=m[0].val;
+ for(i=1;i<m[0].col+1;i++)
+ for(j=1;j<m[0].val+1;j++)
+ {
+ if (m[j].col == i-1)
+            {
+                res[k].row = m[j].col;
+                res[k].col = m[j].row;
+                res[k].val= m[j].val;
+                k++;
+            }
+           }
+          }  
+            
+int main(){
+	
+	
+	struct matrix m[100],res[100];
+	
+	read(m);
+	printMatrix(m);
+	transpose(m,res);
+	printMatrix(res);
+	
+}
